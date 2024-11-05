76f3db0eca5f5db47da31a2cbe414ec6c2101152
kwontaein <zodls1128@gmail.com>
2024-11-05 13:09:42 +0900

    11-05

--- src/ParkingFee.java
+++ src/ParkingFee.java
@@ -1,77 +1,69 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class ParkingFee {
     //자동차 객체 생성
 
     /**
-     * 기본 시간(분)	기본 요금(원)	단위 시간(분)	단위 요금(원) - fees
-     * 180	       5000	        10	       600
-     * <p>
-     * <p>
-     * <p>
-     * <p>
-     * 시간   차량 번호   입출여부 --records
-     * 05:30    0000    IN
-     * o6:00    0000    OUT
-     * <p>
-     * <p>
-     * 출차된 내역이 없다면, 23:59에 출차된 것으로 간주
-     *
-     * @param fees
-     * @param records
+     * @param fees    요금 정보
+     * @param records 기록
      * @return
      */
     public int[] solution(int[] fees, String[] records) {
 
         //다시, 이용시간 + 차량 번호만 저장
         //후 요금 계산
-        int[] answer = {};
-        //차번호 + 시간 저장
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        //차번호 + 요금 저장
-        HashMap<String, Integer> result = new HashMap<>();
+
+        HashMap<String, Integer> inTimes = new HashMap<>();  // 차량 번호 + 입차 시간
+        HashMap<String, Integer> totalTimes = new HashMap<>(); // 차량 번호 + 총 사용 시간
+        HashMap<Integer, Integer> result = new HashMap<>(); // 차량 번호 + 요금
+
+
         for (String s : records) {
-            String[] str = s.split(" ");
-            int time = timeCalculate(str[0]);
-            String carNum = str[1];
+            String[] str = s.split(" "); //문자열 분리
+            int time = timeCalculate(str[0]); //입차 시간 
+            String carNum = str[1]; //차량 번호
 
             if (str[2].equals("IN")) { //입차인경우
-                hashMap.put(carNum, time);
+                inTimes.put(carNum, time); //차량 저장
             } else if (str[2].equals("OUT")) { //출차인경우
-                //리스트에서 번호가 일치하는 차 시간 불러와서 빼기
-                time -= hashMap.get(carNum);
-                hashMap.remove(carNum); // 목록에서 삭제
+                //주차장 사용 시간 계산 후 다시 저장 -- 내 코드
+                /*time -= inTime.get(carNum);
+                totalTimes.put(carNum, time);
+                inTime.remove(carNum);*/
 
-            /*    //만약 이전에 들어온 적이 있다면?
-                if (result.containsKey(carNum)) {
-                    //요금 추가
-                    result.put(carNum, result.get(carNum) + calculateFees(time, fees[0], fees[1], fees[2], fees[3]));
-                } else {
-                    //시간으로 돈 계산 후 결과 목록에 추가
-                    result.put(carNum, calculateFees(time, fees[0], fees[1], fees[2], fees[3]));
-                }*/
+                //수정 코드
+                int inTime = inTimes.remove(carNum);
+                totalTimes.put(carNum, totalTimes.getOrDefault(carNum, 0) + (time - inTime)); // 사용 시간 누적
+
             }
         }
 
-        if (!hashMap.isEmpty()) { //만약 출차하지 않은 차량이 있다면, 마감시간까지의 가격 계산
-            hashMap.forEach((carNum, time) -> {
-                time = timeCalculate("23:59") - time;
-               /* if (result.containsKey(carNum)) {
-                    //요금 추가
-                    result.put(carNum, result.get(carNum) + calculateFees(time, fees[0], fees[1], fees[2], fees[3]));
+        /*if (!inTimes.isEmpty()) { //만약 출차하지 않은 차량이 있다면, 마감시간까지의 사용 시간 계산 후 다시 저장
+            inTimes.forEach((carNum, time) -> {
+                if (!totalTimes.isEmpty()) {
+                    totalTimes.put(carNum, (totalTimes.get(carNum) + timeCalculate("23:59") - time));
                 } else {
-                    //시간으로 돈 계산 후 결과 목록에 추가
-                    result.put(carNum, calculateFees(time, fees[0], fees[1], fees[2], fees[3]));
-                }*/
+                    totalTimes.put(carNum, (timeCalculate("23:59") - time));
+                }
             });
             //마감시간
-        }
-        result.forEach((carNum, price) -> {
-            System.out.println("CarNum : " + carNum + " , Price : " + price);
+        }*/ //--내코드
+
+
+        // 출차하지 않은 차량의 시간 계산 --수정 코드
+        inTimes.forEach((carNum, inTime) -> {
+            int totalTime = totalTimes.getOrDefault(carNum, 0) + (timeCalculate("23:59") - inTime);
+            totalTimes.put(carNum, totalTime);
         });
-        return answer;
+
+        totalTimes.forEach((carNum, usedTime) -> {
+            result.put(Integer.parseInt(carNum), calculateFees(usedTime, fees[0], fees[1], fees[2], fees[3]));
+        });
+
+        return result.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey()) // 차량 번호를 기준으로 정렬
+                .mapToInt(Map.Entry::getValue) // 가격만 추출하여 int 배열로 변환
+                .toArray(); // int 배열로 리턴
     }
 
     //기록 시간 계산
@@ -91,21 +83,21 @@ public class ParkingFee {
      * @return
      */
     private int calculateFees(int usedTime, int leastTime, int price, int increaseTime, int increasePrice) {
-        int mTime;
-        int finalPrice = price;
         if (usedTime <= leastTime) { //기본제공시간 이내에 출차한 경우
-            return finalPrice; //기본요금
+            return price; //기본요금
         } else {
-            //추가로 사용한 시간
-            mTime = usedTime - leastTime;
+             /* //추가로 사용한 시간
+            int mTime = usedTime - leastTime;
             //(추가로 사용한 시간 / 요금 증가 빈도) * 증가요금
-            finalPrice += (mTime / increaseTime) * increasePrice;
+            price += (mTime / increaseTime) * increasePrice;
             //나누어 떨어지지 않으면 올림 처리 (최소요금 1번 추가)
             if (mTime % increaseTime != 0) {
-                finalPrice += increasePrice;
-            }
+                price += increasePrice;
+            }*/ // 내코드
+
+            int extraTime = usedTime - leastTime;
+            return price + ((extraTime + increaseTime - 1) / increaseTime) * increasePrice; // 올림 처리 --수정코드
         }
-        return finalPrice;
     }
 
 }
